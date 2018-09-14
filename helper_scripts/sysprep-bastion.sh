737c46f4c3508522287d205f8eeec390a6ac0e32
Bernd Malmqvist <bernd@lehmann.be>
2018-09-14 18:19:46 +0100

    changed helper scripts

--- helper_scripts/sysprep-bastion.sh
+++ helper_scripts/sysprep-bastion.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/env bash
 
+sudo yum update -y
+sudo yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 sudo yum install -y "@Development Tools" python2-pip openssl-devel python-devel gcc libffi-devel
-
-pip install -I ansible==2.4.3.0
+sudo yum install -y ansible
 git clone -b release-3.9 https://github.com/openshift/openshift-ansible
 
-# pip install -Iv ansible==2.4.1.0
 # git clone -b release-3.7 https://github.com/openshift/openshift-ansible
-
-# Run playbook
-# ANSIBLE_HOST_KEY_CHECKING=False /usr/local/bin/ansible-playbook -i ./inventory.cfg ./openshift-ansible/playbooks/prerequisites.yml
-# ANSIBLE_HOST_KEY_CHECKING=False /usr/local/bin/ansible-playbook -i ./inventory.cfg ./openshift-ansible/playbooks/deploy_cluster.yml
--- helper_scripts/sysprep-openshift.sh
+++ helper_scripts/sysprep-openshift.sh
@@ -1,20 +1,10 @@
 #!/usr/bin/env bash
 
-sudo yum install -y wget git net-tools bind-utils iptables-services bridge-utils bash-completion httpd-tools
 sudo yum update -y
-
-# Note: The step below is not in the official docs, I needed it to install
-# Docker. If anyone finds out why, I'd love to know.
-# See: https://forums.aws.amazon.com/thread.jspa?messageID=574126
+sudo yum install -y wget git net-tools bind-utils iptables-services bridge-utils bash-completion httpd-tools
 sudo yum-config-manager --enable rhui-REGION-rhel-server-extras
+sudo yum install -y docker NetworkManager
 
-# Docker setup. Check the version with `docker version`, should be 1.12.
-sudo yum install -y docker
-
-# Configure the Docker storage back end to prepare and use our EBS block device.
-# https://docs.openshift.org/latest/install_config/install/host_preparation.html#configuring-docker-storage
-# Why xvdf? See:
-# http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/ebs-using-volumes.html
 sudo bash -c 'cat <<EOF > /etc/sysconfig/docker-storage-setup
 DEVS=/dev/xvdf
 VG=docker-vg
@@ -22,5 +12,6 @@ EOF'
 sudo docker-storage-setup
 
 sudo systemctl stop docker
+sudo systemctl enable docker
 sudo rm -rf /var/lib/docker/*
 sudo systemctl restart docker
