fd712c3137822660175c1c8350f674f02efb7582
Bernd Malmqvist <bernd@lehmann.be>
2018-09-14 15:06:01 +0100

    changed helper scripts

--- main.tf
+++ main.tf
@@ -7,12 +7,6 @@ data "aws_availability_zones" "available" {}
 data "template_file" "sysprep-bastion" {
   template = "${file("./helper_scripts/sysprep-bastion.sh")}"
 }
-data "template_file" "sysprep-master" {
-  template = "${file("./helper_scripts/sysprep-master.sh")}"
-}
-data "template_file" "sysprep-infra" {
-  template = "${file("./helper_scripts/sysprep-infra.sh")}"
-}
-data "template_file" "sysprep-worker" {
-  template = "${file("./helper_scripts/sysprep-worker.sh")}"
+data "template_file" "sysprep-openshift" {
+  template = "${file("./helper_scripts/sysprep-openshift.sh")}"
 }
