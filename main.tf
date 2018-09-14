239dba1c7e5a23827446e84cf0c0e97759023fa4
Bernd Malmqvist <bernd@lehmann.be>
2018-09-14 13:07:07 +0100

    change sysprep scripts

--- main.tf
+++ main.tf
@@ -5,14 +5,14 @@ provider "aws" {
 # Declare the data source
 data "aws_availability_zones" "available" {}
 data "template_file" "sysprep-bastion" {
-  template = "${file("./helper_scripts/sysprep-bastion.sh")}"
+  template = "${file("${path}/helper_scripts/sysprep-bastion.sh")}"
 }
 data "template_file" "sysprep-master" {
-  template = "${file("./helper_scripts/sysprep-master.sh")}"
+  template = "${file("${path}/helper_scripts/sysprep-master.sh")}"
 }
 data "template_file" "sysprep-infra" {
-  template = "${file("./helper_scripts/sysprep-infra.sh")}"
+  template = "${file("${path}/helper_scripts/sysprep-infra.sh")}"
 }
 data "template_file" "sysprep-worker" {
-  template = "${file("./helper_scripts/sysprep-worker.sh")}"
+  template = "${file("${path}/helper_scripts/sysprep-worker.sh")}"
 }
