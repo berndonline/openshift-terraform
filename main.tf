90a35eb4b0a9f6356386d1e8a4691e0541ba3843
Bernd Malmqvist <bernd@lehmann.be>
2018-09-14 13:09:11 +0100

    changed path for sysprep scripts

--- main.tf
+++ main.tf
@@ -5,14 +5,14 @@ provider "aws" {
 # Declare the data source
 data "aws_availability_zones" "available" {}
 data "template_file" "sysprep-bastion" {
-  template = "${file("${path}/helper_scripts/sysprep-bastion.sh")}"
+  template = "${file("./helper_scripts/sysprep-bastion.sh")}"
 }
 data "template_file" "sysprep-master" {
-  template = "${file("${path}/helper_scripts/sysprep-master.sh")}"
+  template = "${file("./helper_scripts/sysprep-master.sh")}"
 }
 data "template_file" "sysprep-infra" {
-  template = "${file("${path}/helper_scripts/sysprep-infra.sh")}"
+  template = "${file("./helper_scripts/sysprep-infra.sh")}"
 }
 data "template_file" "sysprep-worker" {
-  template = "${file("${path}/helper_scripts/sysprep-worker.sh")}"
+  template = "${file("./helper_scripts/sysprep-worker.sh")}"
 }
