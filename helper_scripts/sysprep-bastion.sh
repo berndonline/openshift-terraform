09d5740e31e76342f4f173e341689e7deba03521
Bernd Malmqvist <bernd@lehmann.be>
2018-09-14 18:35:52 +0100

    fixed sysprep bastion script

--- helper_scripts/sysprep-bastion.sh
+++ helper_scripts/sysprep-bastion.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
 sudo yum update -y
-sudo yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+sudo yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 sudo yum install -y "@Development Tools" python2-pip openssl-devel python-devel gcc libffi-devel
-sudo yum install -y ansible
 git clone -b release-3.9 https://github.com/openshift/openshift-ansible
-
+sudo yum install -y ansible
 # git clone -b release-3.7 https://github.com/openshift/openshift-ansible
