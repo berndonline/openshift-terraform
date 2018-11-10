9734f6a3f0ee14b0b2e8f4722e6b7bd35e77b63a
Bernd Malmqvist <bernd@lehmann.be>
2018-11-10 16:18:40 +0000

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -9,6 +9,11 @@ resource "aws_instance" "bastion" {
   security_groups = [
     "${aws_security_group.sec_bastion.id}",
   ]
+  root_block_device {
+    volume_type = "gp2"
+    volume_size = "8"
+    delete_on_termination = true
+  }
   associate_public_ip_address = true
   key_name = "${aws_key_pair.bastion.id}"
   user_data = "${data.template_file.sysprep-bastion.rendered}"
@@ -27,6 +32,11 @@ resource "aws_instance" "master1" {
   security_groups = [
     "${aws_security_group.sec_openshift.id}",
   ]
+  root_block_device {
+    volume_type = "gp2"
+    volume_size = "8"
+    delete_on_termination = true
+  }
   key_name = "${aws_key_pair.openshift.id}"
   user_data = "${data.template_file.sysprep-openshift.rendered}"
   tags {
@@ -40,6 +50,11 @@ resource "aws_instance" "worker1" {
   security_groups = [
     "${aws_security_group.sec_openshift.id}",
   ]
+  root_block_device {
+    volume_type = "gp2"
+    volume_size = "8"
+    delete_on_termination = true
+  }
   key_name = "${aws_key_pair.openshift.id}"
   user_data = "${data.template_file.sysprep-openshift.rendered}"
   tags {
@@ -53,6 +68,11 @@ resource "aws_instance" "infra1" {
   security_groups = [
     "${aws_security_group.sec_openshift.id}",
   ]
+  root_block_device {
+    volume_type = "gp2"
+    volume_size = "8"
+    delete_on_termination = true
+  }
   key_name = "${aws_key_pair.openshift.id}"
   user_data = "${data.template_file.sysprep-openshift.rendered}"
   tags {
