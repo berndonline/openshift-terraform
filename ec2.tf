3dce0a51f0bed3bbc4bda187e8ea336d87eb0077
Bernd Malmqvist <bernd@lehmann.be>
2018-11-10 16:14:33 +0000

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -9,10 +9,6 @@ resource "aws_instance" "bastion" {
   security_groups = [
     "${aws_security_group.sec_bastion.id}",
   ]
-  ebs_block_device {
-    device_name  = "bastion"
-    delete_on_termination = "true"
-  }
   associate_public_ip_address = true
   key_name = "${aws_key_pair.bastion.id}"
   user_data = "${data.template_file.sysprep-bastion.rendered}"
@@ -31,10 +27,6 @@ resource "aws_instance" "master1" {
   security_groups = [
     "${aws_security_group.sec_openshift.id}",
   ]
-  ebs_block_device {
-    device_name  = "master1"
-    delete_on_termination = "true"
-  }
   key_name = "${aws_key_pair.openshift.id}"
   user_data = "${data.template_file.sysprep-openshift.rendered}"
   tags {
@@ -48,10 +40,6 @@ resource "aws_instance" "worker1" {
   security_groups = [
     "${aws_security_group.sec_openshift.id}",
   ]
-  ebs_block_device {
-    device_name  = "worker1"
-    delete_on_termination = "true"
-  }
   key_name = "${aws_key_pair.openshift.id}"
   user_data = "${data.template_file.sysprep-openshift.rendered}"
   tags {
@@ -65,10 +53,6 @@ resource "aws_instance" "infra1" {
   security_groups = [
     "${aws_security_group.sec_openshift.id}",
   ]
-  ebs_block_device {
-    device_name  = "infra1"
-    delete_on_termination = "true"
-  }
   key_name = "${aws_key_pair.openshift.id}"
   user_data = "${data.template_file.sysprep-openshift.rendered}"
   tags {
