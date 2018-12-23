b1cd7c6cb8bb09025df9320c461f0a8f5f13722e
Bernd Malmqvist <bernd@lehmann.be>
2018-12-23 22:20:30 +0100

    changed bootstrap

--- helper_scripts/sysprep-openshift.sh
+++ helper_scripts/sysprep-openshift.sh
@@ -4,13 +4,11 @@ sudo yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.
 sudo yum install -y wget git net-tools bind-utils iptables-services bridge-utils bash-completion httpd-tools
 sudo yum-config-manager --enable rhui-REGION-rhel-server-extras
 sudo yum install -y docker NetworkManager
-
 sudo bash -c 'cat <<EOF > /etc/sysconfig/docker-storage-setup
 DEVS=/dev/xvdf
 VG=docker-vg
 EOF'
 sudo docker-storage-setup
-
 sudo systemctl stop docker
 sudo systemctl enable docker
 sudo rm -rf /var/lib/docker/*
