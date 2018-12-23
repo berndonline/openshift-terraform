fdcef56e49379290d1b331591e59a893472c9403
Bernd Malmqvist <bernd@lehmann.be>
2018-12-23 23:42:14 +0100

    Update sysprep-bastion.sh

--- helper_scripts/sysprep-bastion.sh
+++ helper_scripts/sysprep-bastion.sh
@@ -3,6 +3,6 @@ sudo yum update -y
 sudo yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 sudo yum install -y "@Development Tools" python2-pip openssl-devel python-devel gcc libffi-devel
 sudo yum install -y pyOpenSSL python-cryptography python-lxml httpd-tools java-1.8.0-openjdk-headless
-git clone -b release-3.11 https://github.com/openshift/openshift-ansible
+git clone https://github.com/berndonline/openshift-ansible
 sudo pip install --upgrade pip
 sudo pip install 'ansible==2.6.5' passlib
