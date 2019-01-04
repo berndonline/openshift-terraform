b741bb22c8542d5d17882f0f653c4f88595ce5aa
Bernd Malmqvist <bernd@lehmann.be>
2019-01-04 15:22:39 +0000

    Update gce.tf

--- gce.tf
+++ gce.tf
@@ -20,3 +20,21 @@ resource "google_compute_instance" "bastion" {
     sshKeys = "centos:${file(var.bastion_key_path)}"
   }
 }
+resource "google_compute_instance" "master1" {
+  count = 1
+  name = "master1"
+  machine_type = "n1-standard-2"
+  zone = "${var.gcp_zone}"
+  tags = ["master"]
+  boot_disk {
+    initialize_params {
+      image = "${var.gcp_amis}"
+    }
+  }
+  network_interface {
+    subnetwork = "${google_compute_subnetwork.subnet.name}"
+  }
+  metadata {
+    sshKeys = "centos:${file(var.bastion_key_path)}"
+  }
+}
