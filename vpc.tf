cec1f65d99e3d3f045c50ec044d9852efbeca9cb
Bernd Malmqvist <bernd@lehmann.be>
2019-01-04 17:14:41 +0000

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -35,7 +35,7 @@ resource "google_compute_firewall" "firewall-external" {
   network = "${google_compute_network.vpc.name}"
   allow {
       protocol = "tcp"
-      ports = ["22", "80", "443"]
+      ports = ["22", "80", "443", "8443"]
   }
   source_ranges = ["0.0.0.0/0"]
 }
