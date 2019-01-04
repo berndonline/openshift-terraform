12a02c20b5518fc89adc3c1c9f6b62bc32d02f08
Bernd Malmqvist <bernd@lehmann.be>
2019-01-04 16:27:39 +0000

    Create cloudflare.tf

---
+++ cloudflare.tf
@@ -0,0 +1,24 @@
+variable "email" {
+}
+variable "token" {
+}
+variable "domain" {
+}
+provider "cloudflare" {
+  email = "${var.email}"
+  token = "${var.token}"
+}
+resource "cloudflare_record" "console-paas" {
+  domain  = "${var.domain}"
+  name    = "console-paas"
+  value   = "${google_compute_address.master.address}"
+  type    = "A"
+  proxied = false
+}
+resource "cloudflare_record" "wildcard-paas" {
+  domain  = "${var.domain}"
+  name    = "*.paas"
+  value   = "${google_compute_address.infra.address}"
+  type    = "A"
+  proxied = false
+}
