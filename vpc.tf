4ba80acb960f0f4b5eeb617098b196ab51e93162
Bernd Malmqvist <bernd@lehmann.be>
2019-01-04 13:37:12 +0000

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -4,7 +4,7 @@ resource "google_compute_network" "vpc" {
  auto_create_subnetworks = "false"
 }
 resource "google_compute_subnetwork" "subnet" {
- name          = "public_subnet_a"
+ name          = "public-subnet-a"
  ip_cidr_range = "${var.public_subnet_a}"
  network       = "VPC"
  depends_on    = ["google_compute_network.vpc"]
