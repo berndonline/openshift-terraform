2c47cbfce8719fbc1d849b6e5d69cb21a0ccfc19
Bernd Malmqvist <bernd@lehmann.be>
2019-01-04 22:02:19 +0000

    change pass subdomain

--- inventory.tf
+++ inventory.tf
@@ -1,8 +1,8 @@
 data "template_file" "inventory" {
   template = "${file("${path.cwd}/helper_scripts/ansible-hosts.template.txt")}"
   vars {
-    public_subdomain = "paas.hostgate.net"
-    admin_hostname = "console-paas.hostgate.net"
+    public_subdomain = "gcp-paas.hostgate.net"
+    admin_hostname = "console-gcp-paas.hostgate.net"
     master1_hostname = "${google_compute_instance.master1.network_interface.0.network_ip}"
     infra1_hostname = "${google_compute_instance.infra1.network_interface.0.network_ip}"
     worker1_hostname = "${google_compute_instance.worker1.network_interface.0.network_ip}"
