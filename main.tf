b84225ed6cbb500ebc64051896e497bc9784bb86
Bernd Malmqvist <bernd@lehmann.be>
2019-01-04 14:49:29 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,6 +13,3 @@ terraform {
 data "template_file" "sysprep-bastion" {
   template = "${file("./helper_scripts/sysprep-bastion.sh")}"
 }
-data "template_file" "sysprep-openshift" {
-  template = "${file("./helper_scripts/sysprep-openshift.sh")}"
-}
