731a1a8dc65abfc11911230a588d6913e4b9919e
Bernd Malmqvist <bernd@lehmann.be>
2019-01-04 14:28:47 +0000

    update gcp credentials

--- main.tf
+++ main.tf
@@ -1,20 +1,6 @@
-# Specify the provider and access details
-provider "google" {
-  credentials = "${file("./credentials.json")}"
-  project     = "${var.gcp_project}"
-  region      = "${var.gcp_region}"
-}
-# Declare the data source
 data "template_file" "sysprep-bastion" {
   template = "${file("./helper_scripts/sysprep-bastion.sh")}"
 }
 data "template_file" "sysprep-openshift" {
   template = "${file("./helper_scripts/sysprep-openshift.sh")}"
-}
-terraform {
-  backend "gcs" {
-    bucket    = "techbloc-terraform-data"
-    prefix    = "openshift-311"
-    credentials = "credentials.json"
-  }
 }
