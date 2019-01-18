533124cdda305ce73c818adc28325b0ad380e264
Bernd Malmqvist <bernd@lehmann.be>
2019-01-18 18:32:24 +0000

    Update cloudflare.tf

--- cloudflare.tf
+++ cloudflare.tf
@@ -8,14 +8,14 @@ provider "cloudflare" {
   email = "${var.email}"
   token = "${var.token}"
 }
-resource "cloudflare_record" "console-paas" {
+resource "cloudflare_record" "console-aio" {
   domain  = "${var.domain}"
   name    = "console-aio"
   value   = "${aws_lightsail_static_ip.aio_ip.ip_address}"
   type    = "A"
   proxied = false
 }
-resource "cloudflare_record" "wildcard-paas" {
+resource "cloudflare_record" "wildcard-aio" {
   domain  = "${var.domain}"
   name    = "*.aio"
   value   = "${aws_lightsail_static_ip.aio_ip.ip_address}"
