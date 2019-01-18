6d85726b2a6be570fd56cf1ca74bca6d838e4881
Bernd Malmqvist <bernd@lehmann.be>
2019-01-18 18:20:24 +0000

    Update lightsail.tf

--- lightsail.tf
+++ lightsail.tf
@@ -3,8 +3,8 @@ resource "aws_lightsail_key_pair" "aio_key" {
   public_key = "${file("./helper_scripts/id_rsa.pub")}"
 }
 
-resource "aws_lightsail_instance" "aio_instance" {
-  name              = "aio instance"
+resource "aws_lightsail_instance" "aio" {
+  name              = "aio_instance"
   availability_zone = "${var.aws_zone}"
   blueprint_id      = "${var.aws_blueprint}"
   bundle_id         = "${var.aws_bundle}"
@@ -13,9 +13,9 @@ resource "aws_lightsail_instance" "aio_instance" {
 
 resource "aws_lightsail_static_ip_attachment" "aio_ip_attchment" {
   static_ip_name = "${aws_lightsail_static_ip.aio_ip.name}"
-  instance_name  = "${aws_lightsail_instance.aio_instance.name}"
+  instance_name  = "${aws_lightsail_instance.aio.name}"
 }
 
 resource "aws_lightsail_static_ip" "aio_ip" {
-  name = "aio ip"
+  name = "aio_ip"
 }
