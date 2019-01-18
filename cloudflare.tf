0696bd9fdf9153f6c4ba9c677badb0f0f9b81f34
Bernd Malmqvist <bernd@lehmann.be>
2019-01-18 18:31:14 +0000

    Create cloudflare.tf

---
+++ cloudflare.tf
@@ -0,0 +1,24 @@
+variable "email" {
+}
+variable "token" {
+}
+variable "domain" {
+}
+provider "cloudflare" {
+  email = "${var.email}"
+  token = "${var.token}"
+}
+resource "cloudflare_record" "console-paas" {
+  domain  = "${var.domain}"
+  name    = "console-aio"
+  value   = "${aws_lightsail_static_ip.aio_ip.ip_address}"
+  type    = "A"
+  proxied = false
+}
+resource "cloudflare_record" "wildcard-paas" {
+  domain  = "${var.domain}"
+  name    = "*.aio"
+  value   = "${aws_lightsail_static_ip.aio_ip.ip_address}"
+  type    = "A"
+  proxied = false
+}
